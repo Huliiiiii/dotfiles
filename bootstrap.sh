ae0197c1441c2d03b876183c163acff9d9ac0c8a
Huliiiii <134658521+Huliiiiii@users.noreply.github.com>
2025-10-11 07:55:41 +0800

    Add uv

--- bootstrap.sh
+++ bootstrap.sh
@@ -66,3 +66,6 @@ sudo dnf install -y postgresql18-server
 sudo /usr/pgsql-18/bin/postgresql-18-setup initdb
 sudo systemctl enable postgresql-18
 sudo systemctl start postgresql-18
+
+# uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
