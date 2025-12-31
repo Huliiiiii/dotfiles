394c67529b7f0ccbfd97aa5f6e47c350060f1c66
Huliiiiii <134658521+Huliiiiii@users.noreply.github.com>
2025-12-31 20:20:57 +0800

    guix: Update

---
+++ guix-config/home.scm
@@ -0,0 +1,26 @@
+(use-modules
+  (gnu home)
+  (gnu packages)
+  (gnu services)
+  (gnu home services shells)
+  (guix gexp))
+
+(home-environment
+  (packages
+    (specifications->packages
+      (list
+        "git-delta"
+        "glibc-locales"
+        "guile"
+        "guile-wisp"
+        "hello"
+        "just"
+        "nss-certs"
+        "racket")))
+  (services
+    (append
+      (list
+        (simple-service 'extra-profile home-shell-profile-service-type
+          (list (plain-file "shell-profile"
+                 "export GUIX_LOCPATH=\"$HOME/.guix-profile/lib/locale\""))))
+      %base-home-services)))
