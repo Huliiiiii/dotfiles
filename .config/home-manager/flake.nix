24212868515f98ce97925ab9553994355225005c
Huliiiiii <134658521+Huliiiiii@users.noreply.github.com>
2026-01-01 01:14:27 +0800

    home-manager: Add my laptop

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -2,14 +2,13 @@
   description = "Home Manager configuration of Huliiiiii";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     fenix = {
-      url = "github:nix-community/fenix/monthly";
+      url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     tsutsumi = {
@@ -41,8 +40,8 @@
           (
             _: prev:
             let
-              getPkg = src: name: src.packages.${prev.system}.${name};
-              getPkgs = src: src.packages.${prev.system};
+              getPkg = src: name: src.packages.${prev.stdenv.hostPlatform.system}.${name};
+              getPkgs = src: src.packages.${prev.stdenv.hostPlatform.system};
             in
             {
               codex = getPkg inputs.codex "default";
@@ -56,11 +55,104 @@
         config.allowUnfree = true;
       };
 
+      defaultPkgs = with pkgs; [
+        bottom
+        codex
+        delta
+        difftastic
+        dust
+        fastfetch
+        gh
+        helix
+        kdlfmt
+        lazygit
+        mold
+        nil
+        nixd
+        nixfmt-rfc-style
+        nodejs
+        pnpm
+        prettier
+        taplo
+        typos-lsp
+        uv
+        vscode-json-languageserver
+        vtsls
+        wget
+        xclip
+        xsel
+        zellij
+        mergiraf
+      ];
+      codePkgs = with pkgs; [
+        clang
+        claude-code
+        emmet-language-server
+        markdown-oxide
+        marksman
+        opencode
+        oxlint
+        tombi
+      ];
     in
     {
-      homeConfigurations."Huliiiiii" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."huli" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./home.nix ];
+        modules = [
+          ./home.nix
+          (_: {
+            home.username = "huli";
+            home.homeDirectory = "/home/huli";
+            home.packages = defaultPkgs;
+          })
+        ];
       };
+      homeConfigurations."Huliiiiii@pca-workstation" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home.nix
+          (
+            { pkgs, lib, ... }:
+            let
+
+              buildNpmPackage = pkgs.buildNpmPackage.override { nodejs = pkgs.nodejs_24; };
+              pnpm_9 = pkgs.pnpm_9;
+              ccr = import ./modules/ccr.nix {
+                inherit
+                  pkgs
+                  lib
+                  buildNpmPackage
+                  pnpm_9
+                  ;
+              };
+            in
+            {
+              home.username = "Huliiiiii";
+              home.homeDirectory = "/home/Huliiiiii";
+              home.packages =
+                defaultPkgs
+                ++ codePkgs
+                ++ (with pkgs; [
+                  (fenix.complete.withComponents [
+                    "cargo"
+                    "clippy"
+                    "rust-src"
+                    "rustc"
+                    "rustfmt"
+                  ])
+                  rust-analyzer-nightly
+                  tsutsumi.wakatime-ls
+                  podman-compose
+                  yazi
+                ])
+                ++ [
+                  ccr
+                ];
+            }
+          )
+
+        ];
+      };
+
     };
 }
