a21b7c0fcf557cf1da9162e3ea8d878d630904af
Huliiiiii <134658521+Huliiiiii@users.noreply.github.com>
2026-01-04 21:20:30 +0800

    guix: Format

--- guix-config/home.scm
+++ guix-config/home.scm
@@ -1,13 +1,13 @@
-(use-modules
-  (gnu home)
-  (gnu packages)
-  (gnu services)
-  (gnu home services)
-  (gnu home services shells)
-  (guix gexp))
+(use-modules (gnu home)
+             (gnu packages)
+             (gnu services)
+             (gnu home services)
+             (gnu home services shells)
+             (guix gexp))
 
 (define (lines . xs)
-  (let loop ((xs xs) (acc ""))
+  (let loop ([xs xs]
+             [acc ""])
     (if (null? xs)
         acc
         (loop (cdr xs) (string-append acc (car xs) "\n")))))
@@ -15,22 +15,20 @@
 (home-environment
   (packages
     (specifications->packages
-      (list
-        "git-delta"
-        "glibc-locales"
-        "guile"
-        "guile-wisp"
-        "hello"
-        "just"
-        "nss-certs"
-        "racket")))
+      (list "git-delta"
+            "glibc-locales"
+            "guile"
+            "guile-wisp"
+            "hello"
+            "just"
+            "nss-certs"
+            "racket")))
   (services
     (append
-      (list
-        (simple-service 'extra-profile home-shell-profile-service-type
-          (list (plain-file "shell-profile"
-                 (lines
-                   "export GUIX_LOCPATH=\"$HOME/.guix-home/profile/lib/locale\""
-                   "export EDITOR=hx"
-                   "export VISUAL=hx")))))
+      (list (simple-service 'extra-profile home-shell-profile-service-type
+                            (list (plain-file "shell-profile"
+                                              (lines "export GUIX_LOCPATH=\"$HOME/.guix-home/profile/lib/locale\""
+                                                     "export EDITOR=hx"
+                                                     "export VISUAL=hx")))))
       %base-home-services)))
+
