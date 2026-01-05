2e20f65340f8c1046789c87c04f3140e1345299d
Huliiiiii <134658521+Huliiiiii@users.noreply.github.com>
2026-01-05 10:16:24 +0800

    helix: Remove alt bracket binds

--- helix/keymaps.scm
+++ helix/keymaps.scm
@@ -1,5 +1,10 @@
 (require "helix/keymaps.scm")
 
-(define window-ctl '("A-[" "goto_previous_buffer" "A-]" "goto_next_buffer" "A-w" ":buffer-close"))
+(define window-ctl '("A-w" ":buffer-close"))
+(add-global-keybinding (hash "normal" (apply hash window-ctl)
+                             "select" (apply hash window-ctl)))
 
-(add-global-keybinding (hash "normal" (apply hash window-ctl) "select" (apply hash window-ctl)))
+(keymap (global)
+        (normal ("[" (b "goto_previous_buffer"))
+                ("]" (b "goto_next_buffer"))))
+
